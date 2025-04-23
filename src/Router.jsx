e5a1b9cf1269fbdc64903f25e0cf6327bc238c16
Md-Shakhawat-Hosen <shakhawat.hosen.nsu@gmail.com>
2025-04-23 18:29:06 +0600

    added dashboard

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,16 +1,21 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <MainLayout/>,
+    element: <MainLayout />,
     children: [
-        {
-            path: '/',
-            element: <Home/>
-        }
-    ]
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "dashboard",
+        element: <Dashboard/>
+      },
+    ],
   },
 ]);
